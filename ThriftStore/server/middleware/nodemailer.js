db9ffa44ddcf8a86ada710b28365343ddf7c2a12
vaibhav <gholapvaibhav724@gmail.com>
2024-04-01 23:37:40 +0530

    first commit

---
+++ ThriftStore/server/middleware/nodemailer.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer')
+require('dotenv').config()
+
+const email = process.env.EMAIL
+const pass = process.env.EMAIL_PASS
+
+const transporter = nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+        user:email,
+        pass
+    }
+})
+
+const mailOptions ={
+    from:email,
+    to:""
+}
+
+
+module.exports={
+    transporter,
+    mailOptions
+}
