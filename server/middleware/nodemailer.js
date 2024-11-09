6cdf2bf8d1a1aac8f0e6511581a3f714ab5b4738
vaibhav114 <gholapvaibhav724@gmail.com>
2024-11-09 17:16:26 +0530

    nodemailer update

--- server/middleware/nodemailer.js
+++ server/middleware/nodemailer.js
@@ -6,9 +6,16 @@ const pass = process.env.EMAIL_PASS
 
 const transporter = nodemailer.createTransport({
     service:"gmail",
+    port:465,
+    secure:true ,
+    logger:true,
+    secureConnection:false,
     auth:{
         user:email,
         pass
+    },
+    tls:{
+        rejectUnauthorized:true
     }
 })
 
